f4b4d04e78d4f32d53f4fafa23017838c8fef119
Ziad <ziadyasserabtelmiget@gmail.com>
2026-08-13 15:59:34 +0300

    Add Generic Repo

---
+++ GymManagement.DAL/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using GymManagement.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.DAL.Repositories.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+    }
+}
